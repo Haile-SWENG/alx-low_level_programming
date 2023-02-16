da965407398377ac70e96eb89986e4d49131a135
Haile_seng <you@example.com>
2023-02-16 10:45:03 -0800

    it is my first C programming cod

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main aprogame print alin with put function
+ * return:0 (don't stop coding)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+
+}
